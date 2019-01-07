843ef9b77afbb5489e543baf8fdc8df24b425af2
Pat Rice <patrickrice@Patricks-MBP.hsd1.ca.comcast.net>
2019-01-06 20:30:54 -0800

    fixed untracked files

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,23 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+-- CREATE DATABASE cat_db;
+-- USE cat_db;
 
-CREATE TABLE cats
+-- CREATE TABLE cats
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT,
+-- 	name varchar(255) NOT NULL,
+-- 	sleepy BOOLEAN DEFAULT false,
+-- 	PRIMARY KEY (id)
+-- );
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
