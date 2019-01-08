52f6fa6ba6f817592dab35304f0df19ce4efe058
Pat Rice <patrickrice@Patricks-MBP.hsd1.ca.comcast.net>
2019-01-07 23:16:25 -0800

    added heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,3 @@
--- INSERT INTO cats (name) VALUES ('Meeses');
--- INSERT INTO cats (name) VALUES ('Bobbi');
--- INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
--- INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
--- INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
--- INSERT INTO cats (name) VALUES ('Owens');
-
-
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheeseburger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('The Good Morning Breakfast Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Smokehouse Steakburger', false);
